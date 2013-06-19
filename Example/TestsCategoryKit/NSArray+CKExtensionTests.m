fd200a20a9ded47896ea2cb6609e558f2251ec34
Denis Kotenko <d3niskt@gmail.com>
2013-06-19 09:14:42 +0300

    Add SenTestingKit framework and OCHamrest and OCMockito

---
+++ Example/TestsCategoryKit/NSArray+CKExtensionTests.m
@@ -0,0 +1,37 @@
+//
+//  CategoryKit - NSArray+CKExtensionTests.m
+//  Copyright 2013 Ramotion. All rights reserved.
+//
+//  Created by: Denis Kotenko
+//
+
+    // Class under test
+#import "NSArray+CKExtension.h"
+
+    // Collaborators
+
+    // Test support
+#import <SenTestingKit/SenTestingKit.h>
+
+// Uncomment the next two lines to use OCHamcrest for test assertions:
+//#define HC_SHORTHAND
+//#import <OCHamcrestIOS/OCHamcrestIOS.h>
+
+// Uncomment the next two lines to use OCMockito for mock objects:
+//#define MOCKITO_SHORTHAND
+//#import <OCMockitoIOS/OCMockitoIOS.h>
+
+
+@interface NSArray_CKExtensionTests : SenTestCase
+@end
+
+@implementation NSArray_CKExtensionTests {
+    // test fixture ivars go here
+}
+
+//- (void)testExample
+//{
+//    STFail(@"Unit tests are not implemented yet in NSArray_CKExtensionTests");
+//}
+
+@end
